6964558624c780984437d114348ecfa0275e5e3e
Mohamud <detectivemomo1@gmail.com>
2023-03-21 13:14:33 -0400

    working on the leaderboard UI
    -add more code to the leaderboard script.
    - can return to the menu main

--- Pizza_Delivery_Dungeon/Assets/Scripts/FrontEndMenu/LeaderBoard.cs
+++ Pizza_Delivery_Dungeon/Assets/Scripts/FrontEndMenu/LeaderBoard.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeaderBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject return_button = null;
+    public List<Text> Playernames;
+    public List<Text> PlayerMoney;
+
     void Start()
     {
-        
+
+        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+        foreach (GameObject go in allObjects)
+        {
+            if (go.name == "ReturnButton1")
+                return_button = go;
+          
+        }
+
+        return_button.GetComponent<Button>().onClick.AddListener(ReturnButtonOnPress);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    void ReturnButtonOnPress()
+    {
+        Debug.Log("ReturnButtonOnPress2");
+        MenueManager.Instance.our_menu_State = MenueManager.MenuState.MainMenuState;
+        MenueManager.ChangeMenuState();
+    }
+   
+    
 }
